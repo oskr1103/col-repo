bba7a48329a202063f3d76980016d0fdc7ba2c57
Isai Colina <isaicolinapinto@gmail.com>
2021-05-06 14:55:41 -0500

    video player

---
+++ js/index.js
@@ -0,0 +1,8 @@
+import MediaPlayer from "./MediaPlayer.js";
+import AutoPlay from "./plugins/AutoPlay.js";
+
+const video = document.querySelector("video");
+const player = new MediaPlayer({
+  el: video,
+  plugins: [new AutoPlay()],
+});
